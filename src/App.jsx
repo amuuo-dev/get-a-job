060fda9883bc0383bc1c36102d922a09bc5f737f
amuuo-dev <anthonmuuo92@gmail.com>
2024-03-28 06:26:43 +0300

    created a page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Page from "./components/Page";
 const App = () => {
   return (
     <>
@@ -8,6 +9,7 @@ const App = () => {
         title=" Be a React Dev"
         subtitle="Find the React job that fits your skills and needs"
       />
+      <Page />
     </>
   );
 };
