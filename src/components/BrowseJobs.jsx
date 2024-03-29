a0326906802559b0867c198db51e9d51a3983c19
amuuo-dev <anthonmuuo92@gmail.com>
2024-03-29 04:07:29 +0300

    used slice method to unly show 3 jobs instead of all 6 jobs

--- src/components/BrowseJobs.jsx
+++ src/components/BrowseJobs.jsx
@@ -1,6 +1,7 @@
 import jobs from "../jobs.json";
 import JobList from "./JobList";
 const BrowseJobs = () => {
+  const recentJobs = jobs.slice(0, 3);
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -8,7 +9,7 @@ const BrowseJobs = () => {
           Recent Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobs.map((job) => (
+          {recentJobs.map((job) => (
             <JobList key={job.id} job={job} />
           ))}
         </div>
