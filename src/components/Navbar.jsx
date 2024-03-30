ee94319263e8a69a8aa3465179cb508d913d995f
amuuo-dev <anthonmuuo92@gmail.com>
2024-03-30 13:25:42 +0300

    imported Link to prevent refresing while navigating to pages and created a job page and last did routing to pages by importing routing elements from react-router-dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 const Navbar = () => {
   return (
@@ -5,38 +6,35 @@ const Navbar = () => {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-center">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-            <a
-              className="flex flex-shrink-0 items-center mr-4"
-              href="/index.html"
-            >
+            <Link className="flex flex-shrink-0 items-center mr-4" to="/">
               <img src={logo} alt="React logo" className="h-10 w-auto" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 Land A React Job
               </span>
-            </a>
+            </Link>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <a
-                  href="/index.html"
+                <Link
+                  to="/"
                   className="text-white bg-blue-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   {" "}
                   Home
-                </a>
-                <a
-                  href="/jobs.html"
+                </Link>
+                <Link
+                  to="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   {" "}
                   Jobs
-                </a>
-                <a
-                  href="/add-job.html"
+                </Link>
+                <Link
+                  to="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   {" "}
                   Add Job
-                </a>
+                </Link>
               </div>
             </div>
           </div>
