50bddb6ddefb16fca3dfbbae33e33e5e83dfac22
amuuo-dev <anthonmuuo92@gmail.com>
2024-04-01 21:15:54 +0300

    added a go back

--- src/Screen/PageJobSingle.jsx
+++ src/Screen/PageJobSingle.jsx
@@ -1,9 +1,24 @@
 import { useParams, useLoaderData } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const PageJobSingle = () => {
   const { id } = useParams();
   const job = useLoaderData();
 
-  return <h1>{job.title}</h1>;
+  return (
+    <>
+      <section>
+        <div className="m-auto py-6 px-6">
+          <Link
+            to="/jobs/html"
+            className="text-indigo-500 hover:text-indigo-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Job Listings
+          </Link>
+        </div>
+      </section>
+    </>
+  );
 };
 
 const jobLoader = async ({ params }) => {
