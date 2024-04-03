4e77f9a431347237892618969a155b83130a048d
amuuo-dev <anthonmuuo92@gmail.com>
2024-04-03 04:48:30 +0300

    installed toastify for showing confirmation after adding and deleting  a job

--- src/Screen/PageJobSingle.jsx
+++ src/Screen/PageJobSingle.jsx
@@ -1,6 +1,7 @@
 import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 const PageJobSingle = ({ deleteJob }) => {
   const { id } = useParams();
   const job = useLoaderData();
@@ -12,6 +13,7 @@ const PageJobSingle = ({ deleteJob }) => {
     );
     if (!confirm) return;
     deleteJob(jobId);
+    toast.success("Job Deleted Successfully");
     navigate("/jobs");
   };
 
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const MainLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 };
